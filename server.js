54e0241b75e3d42c36209adb25b31001a0fa6a27
Marcola <marcusmagno34@gmail.com>
2024-04-03 15:10:22 -0300

    remoção do http

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import { log } from 'console';
 import app from './src/app.js'
 
-const PORT = 3000
+const PORT = 3000;
 
 app.listen(PORT, () => {
     log(chalk.green(`servidor tá on na porta ${PORT}`))
