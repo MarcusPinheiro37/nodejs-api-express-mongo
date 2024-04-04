373554710651c566ff9518c48004d43f28c29e3d
Marcola <marcusmagno34@gmail.com>
2024-04-04 18:58:58 -0300

    adicionado console.error para o dev

--- src/middlewares/trataErros.js
+++ src/middlewares/trataErros.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function manipulaErros(err, req, res, next) {
+    console.error(err);
+
     if (err instanceof mongoose.Error.CastError){
         res.status(400).send({ message: 'Um ou mais dados fornecidos estão incorreto.' });
     } else {
