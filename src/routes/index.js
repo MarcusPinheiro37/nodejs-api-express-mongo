d4debb2f1959e46694abc781082bc602bd3a3bb0
Marcola <marcusmagno34@gmail.com>
2024-04-04 15:05:59 -0300

    Adicionei cors para usar o swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import livros from './livrosRoutes.js'
+import cors from 'cors';
+import livros from './livrosRoutes.js';
+import autores from './autoresRoutes.js';
 
 const routes = (app) => {
+    // Aqui estamos usando o middleware cors
+    // Esta configuração irá permitir qualquer domínio. Para produção, você deve restringir isso!
+    app.use(cors());
+
     app.route('/').get((req, res) => res.status(200).send('Curso Node JS'));
 
-    app.use(express.json(), livros)
+    // É importante que o middleware json e as rotas sejam usados depois do middleware cors
+    app.use(express.json(), livros, autores);
 };
 
 export default routes;
